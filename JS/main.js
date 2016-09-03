af1c6c14f881fc3b9e42197715109b0c17534c34
Marco Antonio Dominguez <madmzr@gmail.com>
2016-09-02 23:55:48 -0500

    - Added loader - Imrpoved table components - Updated package json - updated index.html - Included React Bootstrap

--- JS/main.js
+++ JS/main.js
@@ -2,12 +2,54 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import Table from './table_component.js';
+
+const navbarInstance = (
+  <Navbar inverse>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="/">Nutriologist</a>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={1} href="#">Link</NavItem>
+        <NavItem eventKey={2} href="#">Link</NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.3}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
+      <Nav pullRight>
+        <NavItem eventKey={1} href="#">Link Right</NavItem>
+        <NavItem eventKey={2} href="#">Link Right</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+ReactDOM.render(navbarInstance, document.getElementById('header'));
+
 
 {
   let totalTable = document.getElementById('totales');
 
   if (!!totalTable)
     ReactDOM.render(<Table/>, totalTable);
-
 }
+
+/* General */
+(function () {
+  let loader = document.getElementById('loader');
+
+  loader.className += ' loaded';
+
+  setTimeout(function () {
+    loader.remove();
+  }, 500);
+})();
--- JS/table_component.js
+++ JS/table_component.js
@@ -4,6 +4,16 @@ import React from 'react';
 import axios from 'axios';
 
  var Table = React.createClass({
+   createTable: function (array) {
+      return array.map(function (item, index) {
+                          return (
+                              <tr key={index}>
+                                <td>{item.name}</td>
+                                <td className="text-right">{item.quantity}</td>
+                              </tr>
+                            )
+                        });
+   },
    getInitialState: function () {
     return {
       elements: []
@@ -27,15 +37,29 @@ import axios from 'axios';
         });
    },
    render: function () {
-     let elements = this.state.elements;
+     let elements = this.state.elements,
+         self = this;
      return (
            <div className="container-fluid">
              {
                elements.map(function (item, index) {
+
+                 console.log(this);
                  return (
                          <div className="row" key={index}>
-                           <div className="xs-12">
+                           <div className="col-xs-12 col-md-8 col-md-offset-2">
                               <h2>{item.name}</h2>
+                              <table>
+                                <thead>
+                                  <tr>
+                                    <th>Nombre</th>
+                                    <th className="text-right">Cantidad en Kg</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  {self.createTable(item.total)}
+                                </tbody>
+                              </table>
                            </div>
                          </div>
                         )
