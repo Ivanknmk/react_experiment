12c57b93705d7e6a9880f75971d4b3c20fd214b5
Marco Antonio Dominguez <madmzr@gmail.com>
2016-09-04 22:01:58 -0500

    Updated header

--- JS/header.js
+++ JS/header.js
@@ -13,19 +13,8 @@ module.exports = {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
+          <NavItem eventKey={2} href="/manage_recipes.html">Administrar recetas</NavItem>
           <NavItem eventKey={1} href="recipes.html">Recetas</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">Link Right</NavItem>
-          <NavItem eventKey={2} href="#">Link Right</NavItem>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
